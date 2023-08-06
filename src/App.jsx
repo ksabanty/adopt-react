76106be72d1397a7a5f1ce79aa2e5886e4c420a4
Kerry Sabanty <kgsabanty@gmail.com>
2023-08-06 18:33:10 -0400

    modal, adopt state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { useState } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Link } from "react-router-dom";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
+import AdoptedPetContext from "./AdoptedPetContext";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -15,9 +17,11 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+  const adoptedPet = useState(null);
   return (
     <BrowserRouter>
     <QueryClientProvider client={queryClient}>
+      <AdoptedPetContext.Provider value={adoptedPet}>
       <header>
         <Link to="/">Adopt Me!</Link>
       </header>
@@ -25,6 +29,7 @@ const App = () => {
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
         </Routes>
+        </AdoptedPetContext.Provider>
     </QueryClientProvider>
     </BrowserRouter>
   )
--- src/Details.jsx
+++ src/Details.jsx
@@ -1,10 +1,16 @@
-import { useParams } from "react-router-dom";
+import { useState, useContext } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
+import AdoptedPetContext from "./AdoptedPetContext";
 import ErrorBoundary from "./ErrorBoundary";
 import fetchPet from "./fetchPet";
 import Carousel from "./Carousel";
+import Modal from "./Modal";
 
 const Details = () => {
+  const [showModal, setShowModal] = useState(false);
+  const navigate = useNavigate();
+  const [_, setAdoptedPet] = useContext(AdoptedPetContext); // eslint-disable-line no-unused-vars
   const { id } = useParams();
   const results = useQuery(["details", id], fetchPet);
 
@@ -30,8 +36,23 @@ const Details = () => {
       <h1>{pet.name}</h1>
       <h2>
         {pet.animal} -{pet.breed} - {pet.city}, {pet.state}
-        <button>Adopt {pet.name}</button>
+        <button onClick={() => setShowModal(true)}>Adopt {pet.name}</button>
         <p>{pet.description}</p>
+        {
+          showModal ? (
+            <Modal>
+              <div>
+                <h1>Would you like to adopt {pet.name}</h1>
+                <div className="buttons">
+                  <button onClick={() => {
+                    setAdoptedPet(pet);
+                    navigate("/");
+                  }}>Yes</button>
+                  <button onClick={() => setShowModal(false)}>No</button>
+                </div>
+              </div>
+            </Modal>
+          ) : null}
       </h2>
     </div>
   );
